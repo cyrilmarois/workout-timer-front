9081944725c81939afbe12caf75e173d7338febd
Cyril Marois <cyrilmarois@gmail.com>
2020-02-03 21:28:36 +0100

    feat(env): set apiUrl config in environment

--- src/app/http-client.service.ts
+++ src/app/http-client.service.ts
@@ -1,29 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { environment } from '../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HttpClientService {
 
-  private BASE_URL = 'http://127.0.0.1:8000/api/v1/';
+  apiUrl: string;
 
   constructor(private httpClient: HttpClient) {
-
+    this.apiUrl = environment.apiUrl;
   }
   get(uri: string, params: any) {
-    return this.httpClient.get(`${this.BASE_URL}` + uri, params);
+    return this.httpClient.get(`${this.apiUrl}` + uri, params);
   }
 
   create(uri: string, body: any) {
-    return this.httpClient.post(`${this.BASE_URL}` + uri, body);
+    return this.httpClient.post(`${this.apiUrl}` + uri, body);
   }
 
   update(uri: string, body: any) {
-    return this.httpClient.patch(`${this.BASE_URL}` + uri, body);
+    return this.httpClient.patch(`${this.apiUrl}` + uri, body);
   }
 
   delete(uri: string, params: any) {
-    return this.httpClient.delete(`${this.BASE_URL}` + uri, params);
+    return this.httpClient.delete(`${this.apiUrl}` + uri, params);
   }
 }

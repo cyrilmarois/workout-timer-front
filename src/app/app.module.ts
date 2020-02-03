5f717ae63698fdfca03706e15214e934e33b0bd6
Cyril Marois <cyrilmarois@gmail.com>
2020-02-03 22:27:24 +0100

    build(dependencies): add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

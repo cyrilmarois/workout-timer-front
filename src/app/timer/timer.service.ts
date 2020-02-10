d72de4055678a39bc81bed247bd5dbe8651fbe21
Cyril Marois <cyrilmarois@gmail.com>
2020-02-10 16:14:07 +0100

    style(layout): set default layout on app component

--- src/app/timer/timer.service.ts
+++ src/app/timer/timer.service.ts
@@ -1,6 +1,7 @@
 import { HttpClientService } from '../core/http-client.service';
 import { Injectable } from '@angular/core';
 import { Timer } from './timer.model';
+import { ITimer } from './timer';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +9,11 @@ import { Timer } from './timer.model';
 export class TimerService {
   private BASE_URI = 'timer';
   private timers: [];
+  private timer: Timer;
 
   constructor(private httpClient: HttpClientService) {}
 
-  async list(options?: any) {
+  async getTimers(options?: any) {
     const res: any =  await this.httpClient.get(`${this.BASE_URI}/`, options)
       .toPromise();
     if (res.data) {
@@ -21,31 +23,35 @@ export class TimerService {
     return this.timers;
   }
 
-  async show(id: number, options?: any) {
-    const data = await this.httpClient.get(`${this.BASE_URI}/` + id, options)
+  async getTimer(id: number, options?: any) {
+    const res: any = await this.httpClient.get(`${this.BASE_URI}/` + id, options)
       .toPromise();
 
-    return data;
+    if (res.id) {
+      this.timer = new Timer(res.id, res.name, res.description);
+    }
+
+    return this.timer;
   }
 
   async update(id: number, body: any) {
-    const data = await this.httpClient.put(`${this.BASE_URI}/` + id , body)
+    const res: any = await this.httpClient.put(`${this.BASE_URI}/` + id , body)
       .toPromise();
 
-    return data;
+    return res;
   }
 
   async create(body: any) {
-    const data = await this.httpClient.post(`${this.BASE_URI}/`, body)
+    const res: any = await this.httpClient.post(`${this.BASE_URI}/`, body)
       .toPromise();
 
-    return data;
+    return res;
   }
 
   async delete(id: number, options?: any) {
-    const data = await this.httpClient.delete(`${this.BASE_URI}/` + id, options)
+    const res: any = await this.httpClient.delete(`${this.BASE_URI}/` + id, options)
       .toPromise();
 
-    return data;
+    return res;
   }
 }
